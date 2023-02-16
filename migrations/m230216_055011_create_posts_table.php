063b8a1a3142b1fce2eb6a71422af641fc5c7c62
fisak <afisak94@gmail.com>
2023-02-16 09:52:53 +0300

    add crud and Post model

---
+++ migrations/m230216_055011_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%posts}}`.
+ */
+class m230216_055011_create_posts_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%posts}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+            'content' => $this->text(),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%posts}}');
+    }
+}
---
+++ views/post/_form.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Post $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="post-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+
+    <div class="form-group mt-3">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
